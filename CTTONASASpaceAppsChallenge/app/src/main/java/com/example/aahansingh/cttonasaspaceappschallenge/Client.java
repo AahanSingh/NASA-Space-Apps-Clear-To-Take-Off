0113e76325f83fb9b924702bc8d42ba15dd57f13
Aahan Singh <aahansingh95@gmail.com>
2016-04-24 12:07:19 +0530

    App finished

---
+++ CTTONASASpaceAppsChallenge/app/src/main/java/com/example/aahansingh/cttonasaspaceappschallenge/Client.java
@@ -0,0 +1,49 @@
+package com.example.aahansingh.cttonasaspaceappschallenge;
+
+import android.os.AsyncTask;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by aahansingh on 24/04/16.
+ */
+class Client extends AsyncTask<String,Void,String> {
+    protected String doInBackground(String... urls) {
+        try {
+            URL url = new URL(urls[0]);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder str = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    str.append(line).append("\n");
+                }
+                br.close();
+            } finally {
+                conn.disconnect();
+                System.out.print(toString()+"\n");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+    protected void onPostExecute(String p) {
+        try {
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
